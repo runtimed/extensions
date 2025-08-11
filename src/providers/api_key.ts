a72ebc475fb56f6bd7113eadeba932e8dc308a84
Anil Kulkarni <akulkarni@anaconda.com>
2025-08-11 16:46:22 -0700

    Add limit/offset to the find api

--- src/providers/api_key.ts
+++ src/providers/api_key.ts
@@ -21,6 +21,11 @@ export type ApiKey = CreateApiKeyRequest & {
   revoked: boolean;
 };
 
+export type ListApiKeysRequest = {
+  limit?: number;
+  offset?: number;
+};
+
 export type ApiKeyProvider = {
   capabilities: Set<ApiKeyCapabilities>;
   overrideHandler?: (context: ProviderContext) => Promise<false | WorkerResponse>; // Any routes the provider wants to fully implement. Return false for any route not handled
@@ -28,7 +33,7 @@ export type ApiKeyProvider = {
   validateApiKey(context: ProviderContext): Promise<Passport>; // Ensure the API key is valid, and returns the Passport. Raise an error otherwise
   createApiKey: (context: AuthenticatedProviderContext, request: CreateApiKeyRequest) => Promise<string>;
   getApiKey: (context: AuthenticatedProviderContext, id: string) => Promise<ApiKey>; // Returns the API key matching the specific api key id
-  listApiKeys: (context: AuthenticatedProviderContext) => Promise<ApiKey[]>; // Return a list of all API keys for a user
+  listApiKeys: (context: AuthenticatedProviderContext, request: ListApiKeysRequest) => Promise<ApiKey[]>; // Return a list of all API keys for a user
   revokeApiKey: (context: AuthenticatedProviderContext, id: string) => Promise<void>; // set the revoked flag for an API key, such that it will no longer be valid
   deleteApiKey: (context: AuthenticatedProviderContext, id: string) => Promise<void>; // Delete an API key from the database
 };
